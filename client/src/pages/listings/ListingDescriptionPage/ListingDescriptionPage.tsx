b0b5c02f5fbcfbd4bf68e5a821a8bbebb2e460e4
Rehman <certifiedrehman@gmail.com>
2022-03-01 21:50:05 -0500

    updated

--- client/src/pages/listings/ListingDescriptionPage/ListingDescriptionPage.tsx
+++ client/src/pages/listings/ListingDescriptionPage/ListingDescriptionPage.tsx
@@ -4,8 +4,12 @@ import {useParams} from "react-router-dom";
 import InvalidPage from "../../../components/InvalidPage/InvalidPage";
 import "./ListingDescriptionPage.scss"
 import Hero from "../../../components/Hero/Hero";
-import {Typography} from "@mui/material";
-import {FavoriteBorderOutlined, ShareOutlined} from "@mui/icons-material";
+import {Button, Tooltip, Typography} from "@mui/material";
+import {
+    FavoriteBorderOutlined,
+    LocationOnOutlined,
+    ShareOutlined
+} from "@mui/icons-material";
 import VerticalButton from "../../../components/VerticalButton/VerticalButton";
 
 function ListingDescriptionPage() {
@@ -32,24 +36,38 @@ function ListingDescriptionPage() {
         return !listing ? <InvalidPage className={'page-content'}/> :
             <div className={'page-content listing-description-page'}>
                 <div className={'listing-main'}>
-                    <Hero className={'listing-gallery-swiper'}>
-                        {listing.photos.map((photo:string, index:number) => {
-                            return <img key={index} src={photo} alt={`${listing.title} Photo ${index + 1}`}/>
-                        })}
-                    </Hero>
                     <div className={'listing-header'}>
                         <div className={'header-text'}>
-                            <Typography variant={'h5'} fontWeight={'medium'}>{listing.title}</Typography>
-                            <Typography variant={'body2'} color={'dimgray'}>6 guests · {listing.bedrooms} bedrooms · {listing.beds} beds · {listing.baths} bath</Typography>
+                            <Typography variant={'h5'} fontWeight={'medium'}>{titleCase(listing.title)}</Typography>
+                            <Tooltip title={'Approximate Location'}>
+                                <Button sx={{
+                                    justifyContent: 'flex-start',
+                                }}><LocationOnOutlined/>{listing.location.city}, {listing.location.province}</Button>
+                            </Tooltip>
                         </div>
                         <div className={'header-buttons'}>
                             <VerticalButton icon={<ShareOutlined color={'disabled'}/>} label={'Share'}/>
                             <VerticalButton icon={<FavoriteBorderOutlined color={'disabled'}/>} label={'Save'} outlined/>
                         </div>
                     </div>
-                    <div className={'listing-description'}>
+                    <Hero className={'listing-gallery-swiper'}>
+                        {listing.photos.map((photo:string, index:number) => {
+                            return <img key={index} src={photo} alt={`${listing.title} Photo ${index + 1}`}/>
+                        })}
+                    </Hero>
+                    <div className={'border-bottom'}>
+                        <Typography variant={'h6'}>
+                            {titleCase(listing.listingType)} in a {titleCase(listing.type)} in {titleCase(listing.location.city)}
+                        </Typography>
+                        <Typography variant={'body2'} color={'dimgray'}>6 guests · {listing.bedrooms} bedrooms · {listing.beds} beds · {listing.baths} bath</Typography>
+                    </div>
+                    <div className={'listing-description border-bottom'}>
                         <Typography variant={'h6'}>Description</Typography>
                         <Typography variant={'body1'}>{listing.description}</Typography>
+                    </div>
+                    <div className={'listing-reviews border-bottom'}>
+                        <Typography variant={'h6'}>Reviews</Typography>
+                        <Typography variant={'body1'}>15 Reviews</Typography>
                     </div>
                 </div>
                 <div className={'listing-checkout-card'}>
