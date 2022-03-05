5cf5931c69215a1eb0f293b34a03aa7b90eeaea2
Rehman <certifiedrehman@gmail.com>
2022-03-04 21:52:37 -0500

    using Stack

--- client/src/components/ListingCard/ListingCard.tsx
+++ client/src/components/ListingCard/ListingCard.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './ListingCard.scss'
 import {Swiper, SwiperSlide} from "swiper/react";
 import {A11y, Pagination} from "swiper";
-import {CardActionArea, Rating, Tooltip} from "@mui/material";
+import {CardActionArea, Rating, Stack, Tooltip} from "@mui/material";
 import {Link} from "react-router-dom";
 import {BathroomOutlined, BedroomParentOutlined} from "@mui/icons-material";
 
@@ -41,34 +41,34 @@ function ListingCard({listing, className, ...rest}: IProps) {
                         <div className="listing-card-info-top">
                             <p style={{fontWeight: 500}}>{listing.title}</p>
                             <div className="listing-card-info-rating">
-                                <Tooltip title={'Rating: ' + listing.rating}>
-                                    <span>
-                                        <Rating name="read-only" value={listing.rating} precision={0.5} readOnly size={'small'}/>
-                                    </span>
+                                <Tooltip title={'Rating: ' + listing.rating.average}>
+                                    <Stack direction={"row"} color={"darkgrey"}>
+                                        <Rating name="read-only" value={listing.rating.average} precision={0.5} readOnly size={'small'}/> / {listing.rating.count}
+                                    </Stack>
                                 </Tooltip>
                                 <b>{`${titleCase(listing.listingType)}`}</b>
                             </div>
                         </div>
                         <div className="listing-card-info-mid">
-                            {`${listing.beds * 2} guests · ${listing.bedrooms} bedrooms · ${listing.amenities.join(' · ')}`}
+                            {`${listing.guests} guests · ${listing.bedrooms} bedrooms · ${listing.amenities.join(' · ')}`}
                         </div>
                         <div className="listing-card-info-bottom">
                             <div className={"listing-card-info-data"}>
                                 <div className={'listing-card-info-icons'}>
                                     {listing.beds ?
-                                        <Tooltip title={'Number of Beds: ' + listing.beds}>
-                                    <span>
-                                        <BedroomParentOutlined/>
-                                        {listing.beds}
-                                    </span>
+                                        <Tooltip title={'Beds: ' + listing.beds}>
+                                            <Stack direction={"row"} alignItems={'center'}>
+                                                <BedroomParentOutlined/>
+                                                {listing.beds}
+                                            </Stack>
                                         </Tooltip> : null}
 
                                     {listing.baths ?
-                                        <Tooltip title={'Number of Bathrooms: ' + listing.baths}>
-                                    <span>
-                                        <BathroomOutlined/>
-                                        {listing.baths}
-                                    </span>
+                                        <Tooltip title={'Bathrooms: ' + listing.baths}>
+                                            <Stack direction={"row"} alignItems={'center'}>
+                                                <BathroomOutlined/>
+                                                {listing.baths}
+                                            </Stack>
                                         </Tooltip> : null}
                                 </div>
                             </div>
