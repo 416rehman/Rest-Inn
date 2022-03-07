180a3bacdd9ce2800935cfa071ee491b225dc463
Rehman <certifiedrehman@gmail.com>
2022-03-07 01:29:49 -0500

    Snackbar wrapper

---
+++ client/src/components/NotificationSnackBar/Notification.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {IconButton, Snackbar} from "@mui/material";
+import {Close} from "@mui/icons-material";
+interface IProps {
+    message: string;
+}
+function Notification(props: IProps) {
+    const [open, setOpen] = React.useState(true);
+
+    const handleClose = (event: React.SyntheticEvent | Event, reason?: string) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+
+        setOpen(false);
+    };
+
+    const action = (
+        <React.Fragment>
+            <IconButton
+                size="small"
+                aria-label="close"
+                color="inherit"
+                onClick={handleClose}
+            >
+                <Close fontSize="small" />
+            </IconButton>
+        </React.Fragment>
+    );
+
+    return (
+        <Snackbar
+            open={open}
+            autoHideDuration={6000}
+            onClose={handleClose}
+            message={props.message}
+            action={action}
+        />
+    );
+}
+
+export default Notification;
