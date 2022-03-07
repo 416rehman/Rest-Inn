3d0de3ce641743f1fb6760f765be7d98a57a44ff
Rehman <certifiedrehman@gmail.com>
2022-03-07 01:20:59 -0500

    added auto-renewing session to apiURL

--- client/src/helpers/utils.ts
+++ client/src/helpers/utils.ts
@@ -1,18 +1,37 @@
+import store from "../redux/store";
+import jwtDecode from "jwt-decode";
+import {renewSession} from "./userAPI.helper";
+
 enum API_GATEWAY {
-  PROD = `https://rest-inn.herokuapp.com`,
-  DEV = `http://localhost:8080`
+    PROD = `https://rest-inn.herokuapp.com`,
+    DEV = `http://localhost:8080`
 }
 
-global.apiURL = (path: string, query?:string) => `${API_GATEWAY.DEV}${path||''}${query ? '?' + query : ''}`
+/**
+ * Builds the API Gateway URL, and renews the accessToken if needed
+ * @param path
+ * @param query
+ */
+global.apiURL = (path: string, query?: string) => {
+    // if accessToken is expired, auto-renew it
+    const accessToken = store.getState().accessToken;
+    if (accessToken) {
+        const decodedToken: any = jwtDecode(accessToken || "");
+        if (decodedToken && decodedToken.exp < Date.now() / 1000)
+          renewSession();
+    }
+
+    return `${API_GATEWAY.DEV}${path || ''}${query ? '?' + query : ''}`;
+}
 
 global.titleCase = (string) => {
-  return string.replace(/\w\S*/g, (txt) => {
-    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-  })
+    return string.replace(/\w\S*/g, (txt) => {
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    })
 }
 
 global.randomHexColor = () => {
-  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+    return '#' + Math.floor(Math.random() * 16777215).toString(16);
 }
 
 export {};
