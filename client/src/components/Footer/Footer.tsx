7ef7a08af4002d27811f5698400e61eb7b0f9f8a
Rehman <certifiedrehman@gmail.com>
2022-03-07 02:09:44 -0500

    added auth-aware links

--- client/src/components/Footer/Footer.tsx
+++ client/src/components/Footer/Footer.tsx
@@ -1,60 +1,71 @@
 import React from 'react';
-import {Button, IconButton} from "@mui/material";
+import {Button, IconButton, Stack, Tooltip} from "@mui/material";
 import {
-    FacebookOutlined,
-    HomeOutlined,
-    Instagram,
+    HomeOutlined, HttpRounded,
+    Instagram, LaunchRounded,
     Twitter,
     YouTube
 } from "@mui/icons-material";
 import "./Footer.scss"
 import {Link} from "react-router-dom";
+import {useSelector} from "react-redux";
+import {RootState} from "../../@typings/auth";
 
 function Footer() {
+    const {isAuthenticated} = useSelector((state: RootState) => state);
+
     return (
-        <div className={'footer'}>
-            <div className={'listings-footer'}>
+        <Stack className={'footer'} direction={'row'} justifyContent={'space-between'} sx={{
+            padding: '1rem',
+            backgroundColor: '#F7F7F7',
+            borderTop: '1px solid #e5e5e5',
+            '@media screen and (max-width: 600px)': {
+                flexDirection: 'column',
+            }
+        }}>
+            <Stack className={'listings-footer'} direction={'row'}>
                 <div className={'footer-links'}>
                     <Link to={'/'}><Button startIcon={<HomeOutlined/>}>Home</Button></Link>
-                    <Link to={'/listings'}><Button>All Listings</Button></Link>
-                    <Link to={'/auth/login'}><Button >Log In</Button></Link>
-                    <Link to={'/auth/signup'}><Button>Sign Up</Button></Link>
-                    <Link to={'/auth/forgot'}><Button>Forgot Password</Button></Link>
+                    <Link to={'/listings'}><Button>Listings</Button></Link>
+                    {!isAuthenticated && <>
+                        <Link to={'/auth/login'}><Button>Log In</Button></Link>
+                        <Link to={'/auth/signup'}><Button>Sign Up</Button></Link>
+                        <Link to={'/auth/forgot'}><Button>Forgot Password</Button></Link>
+                    </>}
+                    <a href={apiURL('')}><Button endIcon={<LaunchRounded/>}>Developers / API</Button></a>
                 </div>
-            </div>
-            <div className={'social-footer'}>
-                    <p>
-                        <span>
-                            <a href={'https://www.facebook.com/'}>
-                                <IconButton aria-label={'facebook'}>
-                                    <FacebookOutlined/>
-                                </IconButton>
-                            </a>
-                        </span>
-                        <span>
-                            <a href={'https://www.instagram.com/'}>
-                                <IconButton aria-label={'instagram'}>
-                                    <Instagram/>
-                                </IconButton>
-                            </a>
-                        </span>
-                        <span>
-                            <a href={'https://www.twitter.com/'}>
-                                <IconButton aria-label={'twitter'}>
-                                    <Twitter/>
-                                </IconButton>
-                            </a>
-                        </span>
-                        <span>
-                            <a href={'https://www.youtube.com/'}>
-                                <IconButton aria-label={'youtube'}>
-                                    <YouTube/>
-                                </IconButton>
-                            </a>
-                        </span>
-                    </p>
-            </div>
-        </div>
+            </Stack>
+            <Stack className={'social-footer'} direction={'row'} justifyContent={'end'} alignItems={'center'}>
+                <Tooltip title={'API'}>
+                    <a href={apiURL('')}>
+                        <IconButton aria-label={'api'}>
+                            <HttpRounded/>
+                        </IconButton>
+                    </a>
+                </Tooltip>
+                <Tooltip title={'Instagram'}>
+                    <a href={'https://www.instagram.com/'}>
+                        <IconButton aria-label={'instagram'}>
+                            <Instagram/>
+                        </IconButton>
+                    </a>
+                </Tooltip>
+                <Tooltip title={'Twitter'}>
+                    <a href={'https://www.twitter.com/'}>
+                        <IconButton aria-label={'twitter'}>
+                            <Twitter/>
+                        </IconButton>
+                    </a>
+                </Tooltip>
+                <Tooltip title={'Youtube'}>
+                    <a href={'https://www.youtube.com/'}>
+                        <IconButton aria-label={'youtube'}>
+                            <YouTube/>
+                        </IconButton>
+                    </a>
+                </Tooltip>
+            </Stack>
+        </Stack>
     );
 }
 
