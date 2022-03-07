071e85cd47b2f7ad992bee9a312ebfe36c0c173b
Rehman <certifiedrehman@gmail.com>
2022-03-07 02:09:57 -0500

    Using typography

--- client/src/pages/notFound/NotFoundPage.tsx
+++ client/src/pages/notFound/NotFoundPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Link} from "react-router-dom";
-import {Button} from "@mui/material";
+import {Button, Stack, Typography} from "@mui/material";
 import { HomeOutlined} from "@mui/icons-material";
 import { useNavigate  } from 'react-router-dom';
 
@@ -9,11 +9,17 @@ function NotFoundPage()
     let history = useNavigate();
 
     return (
-        <div className={'page-content'}>
-            <h1>Uh oh! We couldn't find the page you are looking for 😣 </h1>
-            <p>The link may be broken, or the page may have been removed. Check to see if the link you're trying to open is correct.</p>
-            <Link to={'/'}><Button variant="contained" disableElevation startIcon={<HomeOutlined/>}>Go to Home</Button></Link> <Button onClick={()=>{history(-1)}}>Go Back</Button>
-        </div>
+        <Stack className={'page-content'} alignItems={'center'}>
+            <Stack spacing={'1rem'}>
+                <Typography variant={'h4'} fontWeight={600}>Uh oh! We couldn't find the page you were looking for... 😣</Typography>
+                <Typography variant={'body1'}>
+                    The link may be broken, or the page may have been removed. Check to see if the link you're trying to open is correct.</Typography>
+                <Stack direction={'row'} spacing={'1rem'}>
+                    <Link to={'/'}><Button variant="contained" disableElevation startIcon={<HomeOutlined/>}>Go to Home</Button></Link>
+                    <Button onClick={()=>{history(-1)}}>Go Back</Button>
+                </Stack>
+            </Stack>
+        </Stack>
     );
 }
 
