5635a6997e94af5c0ff7e29ca87cc6c78fc91ca6
Rehman <certifiedrehman@gmail.com>
2022-03-08 03:24:27 -0500

    Added Filtering

--- client/src/pages/listings/ListingsPage.tsx
+++ client/src/pages/listings/ListingsPage.tsx
@@ -1,72 +1,78 @@
-import React, {useEffect} from 'react';
+import {Fragment, useEffect, useState} from 'react';
 import ListingCard from "../../components/ListingCard/ListingCard";
 import './ListingsPage.scss';
-import {useSearchParams} from 'react-router-dom'
 import axios from 'axios'
 import InvalidPage from "../../components/InvalidPage/InvalidPage";
-import {Pagination, Skeleton} from "@mui/material";
+import {Pagination, Skeleton, Stack} from "@mui/material";
 import {ListingPartial} from "../../@typings/listings";
+import {useSearchParams} from "react-router-dom";
+import ListingsFilterBar from "../../components/Filters/ListingsFilterBar";
 
-//TODO: Add Filtering
 
 function ListingsPage() {
     const [searchParams] = useSearchParams();
-    const [listings, setListings] = React.useState<ListingPartial[]>([]);
-    const [totalPages, setTotalPages] = React.useState(0);
-    const [currentPage, setCurrentPage] = React.useState(1);
-    const [loading, setLoading] = React.useState(true);
+    const [appliedFilters, setAppliedFilters] = useState<any>({});
 
-    useEffect(()=>{
-        setCurrentPage(parseInt(searchParams.get('page') || '1'));
+    const [listings, setListings] = useState<ListingPartial[]>([]);
+    const [counts, setCounts] = useState<{ count: number, pages: number }>({count: 0, pages: 0});
+
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const params = Object.fromEntries(searchParams);
+        setAppliedFilters(params);
     }, [])
 
-    useEffect(()=> {
-        searchParams.set('page', currentPage.toString());
-        const filter = searchParams.toString()
-        axios.get(apiURL('/properties', filter))
+    useEffect(() => {
+        // Create a new URLSearchParams object from the applied filters, convert to query string, and update the URL
+        const newFilter = new URLSearchParams(appliedFilters);
+        window.history.pushState(null, '', `?${newFilter.toString()}`);
+
+        // Fetch listings with the applied filters
+        axios.get(apiURL('/properties', newFilter.toString()))
             .then(res => {
                 setListings(res.data.data)
-                setTotalPages(res.data.pagination.totalPages)
+                setCounts({
+                    count: res.data.pagination.count,
+                    pages: res.data.pagination.totalPages
+                })
+            })
+            .catch(() => {
+                setListings([])
+            })
+            .finally(() => {
                 setLoading(false)
             })
-            .catch(err => {
-                console.log(err)
-                setLoading(false)
-            })
-    }, [searchParams, currentPage])
-
-    const changePage = (event: React.ChangeEvent<unknown>, value: number) => {
-        setCurrentPage(value)
-    }
+    }, [appliedFilters])
 
     return loading ? (
         <div className={'listings-page page-content'}>
-            <Skeleton variant={'rectangular'} height={180} style={{
-                borderRadius: '25px',
-            }}/>
-            <Skeleton variant={'rectangular'} height={180} style={{
-                borderRadius: '25px',
-            }}/>
-            <Skeleton variant={'rectangular'} height={180} style={{
-                borderRadius: '25px',
-            }}/>
-            <Skeleton variant={'rectangular'} height={180} style={{
-                borderRadius: '25px',
-            }}/>
+            <ListingsFilterBar filters={appliedFilters} setFiltersHandler={setAppliedFilters}/>
+            {Array(5).map((_, i) => (
+                <Skeleton key={i} variant={'rectangular'} height={180} style={{
+                    borderRadius: '25px',
+                }}/>
+            ))}
         </div>
     ) : (
         <div className={'listings-page page-content'}>
+            <Stack><ListingsFilterBar filters={appliedFilters} setFiltersHandler={setAppliedFilters}/></Stack>
             {listings.length ?
-                (<React.Fragment>
+                (<Fragment>
                     <div className={'listings-container'}>
-                        {listings.map(listing => <ListingCard key={listing._id}  listing={listing}/>)}
+                        {listings.map(listing => <ListingCard key={listing._id} listing={listing}/>)}
                     </div>
-                    <Pagination count={totalPages} page={currentPage} shape="rounded" onChange={changePage} />
-                </React.Fragment>)
-                    :
+                    <Pagination count={counts.pages}
+                                page={parseInt(appliedFilters.page) || 1}
+                                shape="rounded"
+                                onChange={(event, value) => {
+                                    setAppliedFilters({...appliedFilters, page: value})
+                                }}
+                    />
+                </Fragment>)
+                :
                 <InvalidPage/>
             }
-
         </div>
     )
 }
