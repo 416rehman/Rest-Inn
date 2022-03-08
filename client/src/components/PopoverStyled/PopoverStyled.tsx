b4efa2495159f67137ce1c1e39bcbe7cc21f2d0b
Rehman <certifiedrehman@gmail.com>
2022-03-08 04:44:31 -0500

    Changed Popover to use Chips

--- client/src/components/PopoverStyled/PopoverStyled.tsx
+++ client/src/components/PopoverStyled/PopoverStyled.tsx
@@ -1,17 +1,20 @@
 import * as React from 'react';
 import Popover from '@mui/material/Popover';
-import Button from '@mui/material/Button';
+import { Chip} from "@mui/material";
 
 interface IProps {
     title: string;
     children: React.ReactNode;
-    variant?: 'text' | 'outlined' | 'contained';
+    variant?: 'filled' | 'outlined';
+    onDelete?: () => void;
+    icon?: React.ReactElement;
 }
-export default function PopoverStyled({ title, children, variant }: IProps) {
-    const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(null);
 
-    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        setAnchorEl(event.currentTarget);
+export default function PopoverStyled({title, children, variant, onDelete, icon}: IProps) {
+    const [anchorEl, setAnchorEl] = React.useState<HTMLElement | null>(null);
+
+    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event?.currentTarget);
     };
 
     const handleClose = () => {
@@ -23,9 +26,18 @@ export default function PopoverStyled({ title, children, variant }: IProps) {
 
     return (
         <div>
-            <Button aria-describedby={id} variant={variant || "outlined"} onClick={handleClick}>
-                {title || 'More'}
-            </Button>
+            <Chip
+                aria-describedby={id}
+                label={title}
+                color={'primary'}
+                icon={icon}
+                onClick={handleClick}
+                onDelete={onDelete}
+                variant={variant || "outlined"}
+                sx={{
+                    pl: icon ? '0.5rem' : '0',
+                }}
+            />
             <Popover
                 id={id}
                 open={open}
