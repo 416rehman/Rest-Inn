398cbbcac314fd94b2d722eab7461f0cd7cc0e5e
Rehman <certifiedrehman@gmail.com>
2022-03-08 03:22:16 -0500

    Toggle Filter

---
+++ client/src/components/Filters/filterTypes/ToggleFilter.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { FormControlLabel, IconButton, Paper, Stack, Typography} from "@mui/material";
+import SwitchStyled from "../../SwitchStyled";
+import {CloseRounded} from "@mui/icons-material";
+
+interface IProps {
+    label?: string;
+    name: string;
+    filters: {[key:string]: string},
+    setFiltersHandler: (filters: {[key:string]: string}) => void;
+    clearOnFalse?: boolean;
+}
+
+function ToggleFilter({label, name, filters, setFiltersHandler, clearOnFalse = true}: IProps) {
+
+    const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (clearOnFalse && !event.target.checked) clearFilter()
+        else setFiltersHandler({...filters, [name]: event.target.checked ? 'true' : 'false'})
+    };
+
+    const clearFilter = () => {
+        const newFilters = {...filters};
+        delete newFilters[name];
+        setFiltersHandler(newFilters);
+    };
+
+    const isActive = (name: string, filters: {[key: string]: string}) => {
+        return filters[name] != '' && filters[name] != undefined && filters[name] != null
+    }
+
+    return (
+        <Stack>
+            <Paper sx={{pl: '1rem'}} variant={'outlined'}>
+                <FormControlLabel control={
+                    <SwitchStyled size={'medium'} checked={JSON.parse(filters[name] || 'false')} name={name} onChange={handleOnChange}/>
+                } label={
+                    <Typography variant={'subtitle2'}>{label}</Typography>
+                } />
+                {isActive(name, filters) && <IconButton onClick={clearFilter} aria-label={'clear'} size={'small'}>
+                    <CloseRounded/>
+                </IconButton>}
+            </Paper>
+
+        </Stack>
+    );
+}
+
+export default ToggleFilter;
