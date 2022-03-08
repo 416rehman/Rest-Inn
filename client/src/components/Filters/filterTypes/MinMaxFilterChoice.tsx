c4ddbbaeb1b40845d32cb2ef1fcc9a9b6609ade5
Rehman <certifiedrehman@gmail.com>
2022-03-08 04:44:00 -0500

    added Icons to Choice

--- client/src/components/Filters/filterTypes/MinMaxFilterChoice.tsx
+++ client/src/components/Filters/filterTypes/MinMaxFilterChoice.tsx
@@ -1,6 +1,6 @@
 import PopoverStyled from "../../PopoverStyled/PopoverStyled";
 import {Button, Stack, TextField} from "@mui/material";
-import {useEffect, useState} from "react";
+import {ReactElement, useEffect, useState} from "react";
 
 interface IProps {
     title: string;
@@ -8,13 +8,14 @@ interface IProps {
         name: string;
         label: string;
     }[];
-    filters: {[key:string]: string},
-    setFiltersHandler: (filters: {[key:string]: string}) => void;
+    filters: { [key: string]: string },
+    setFiltersHandler: (filters: { [key: string]: string }) => void;
     minValue?: number;
     maxValue?: number;
+    icon?: ReactElement;
 }
 
-export default function MinMaxFilterChoice({title, fields, filters, setFiltersHandler, minValue, maxValue}: IProps) {
+export default function MinMaxFilterChoice({title, fields, filters, setFiltersHandler, minValue, maxValue, icon}: IProps) {
     const [pendingFilters, setPendingFilters] = useState<{ [key: string]: string }>({});
 
     useEffect(() => {
@@ -30,13 +31,13 @@ export default function MinMaxFilterChoice({title, fields, filters, setFiltersHa
     }
 
     // Checks if atleast one of the provided filters is set in the provided filter object
-    const isActive = (names: string[], filters: {[key: string]: string}) => {
+    const isActive = (names: string[], filters: { [key: string]: string }) => {
         return names.some(name => filters[name] != '' && filters[name] != undefined && filters[name] != null)
     }
 
     // Returns 'contained' if atleast one of the provided filters is set in the provided filter object, else returns 'outlined'
     const handleVariant = (names: string[]) => {
-        return isActive(names, filters) ? 'contained' : 'outlined'
+        return isActive(names, filters) ? 'filled' : 'outlined'
     }
 
     // Clear the provided filters from the filters object
@@ -49,7 +50,7 @@ export default function MinMaxFilterChoice({title, fields, filters, setFiltersHa
         setFiltersHandler(newFilters);
     }
 
-    const onInputHandler= (e: any) => {
+    const onInputHandler = (e: any) => {
         // make sure the value is within the min and max values
         const {name, value} = e.target;
         if (maxValue != undefined && value > maxValue) e.target.value = maxValue;
@@ -57,20 +58,31 @@ export default function MinMaxFilterChoice({title, fields, filters, setFiltersHa
         setPendingFilters({...pendingFilters, [name]: value})
     }
 
-    return (<PopoverStyled title={title} variant={handleVariant(fieldNames)}>
-        <form onSubmit={(e) => { e.preventDefault();  setFiltersHandler(pendingFilters);}}>
-            <Stack gap={'0.5rem'} padding={'1rem 0.5rem 0.5rem 0.5rem'}>
-                <Stack direction={'row'} gap={'1rem'} flexWrap={'wrap'}>
-                    {fields.map(field => (
-                        <TextField key={field.name} label={field.label} type={'number'} name={field.name} size={'small'}
-                                   value={pendingFilters[field.name]} onChange={onChangeHandler} onInput={onInputHandler}/>
-                    ))}
+    return (
+        <PopoverStyled title={title} variant={handleVariant(fieldNames)} icon={icon}
+                       onDelete={isActive(fieldNames, filters) ? () => {clearFilter(fieldNames)} : undefined}
+
+        >
+            <form onSubmit={(e) => {
+                e.preventDefault();
+                setFiltersHandler(pendingFilters);
+            }}>
+                <Stack gap={'0.5rem'} padding={'1rem 0.5rem 0.5rem 0.5rem'}>
+                    <Stack direction={'row'} gap={'1rem'} flexWrap={'wrap'}>
+                        {fields.map(field => (
+                            <TextField key={field.name} label={field.label} type={'number'} name={field.name}
+                                       size={'small'}
+                                       value={pendingFilters[field.name]} onChange={onChangeHandler}
+                                       onInput={onInputHandler}/>
+                        ))}
+                    </Stack>
+                    <Stack direction={'row'} gap={'0.5rem'}>
+                        {isActive(fieldNames, pendingFilters) &&
+                            <Button type={'submit'} variant={'outlined'} color={'primary'}>Apply</Button>}
+                        {isActive(fieldNames, filters) &&
+                            <Button color={'error'} onClick={() => clearFilter(fieldNames)}>Clear</Button>}
+                    </Stack>
                 </Stack>
-                <Stack direction={'row'} gap={'0.5rem'}>
-                    {isActive(fieldNames, pendingFilters) && <Button type={'submit'} variant={'outlined'} color={'primary'}>Apply</Button>}
-                    {isActive(fieldNames, filters) && <Button color={'error'} onClick={() => clearFilter(fieldNames)}>Clear</Button>}
-                </Stack>
-            </Stack>
-        </form>
-    </PopoverStyled>)
+            </form>
+        </PopoverStyled>)
 }
