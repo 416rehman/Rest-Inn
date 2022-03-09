2ba8089a48af0f376bcd91f3bb9d184b91cf10ea
Rehman <certifiedrehman@gmail.com>
2022-03-09 04:18:55 -0500

    generic error component
    Took 11 seconds

---
+++ client/src/components/Errors/ErrorGeneric.tsx
@@ -0,0 +1,47 @@
+import React, {ReactElement} from 'react';
+import {Alert, AlertTitle, Button, Divider, Stack} from "@mui/material";
+import {apiURL} from "../../services/helper.service";
+import {LaunchOutlined} from "@mui/icons-material";
+interface IProps {
+    children?: ReactElement | ReactElement[];
+    message ?: string;
+    title?: string;
+    buttonText?: string;
+    buttonHandler?: () => void;
+    severity?: 'error' | 'info' | 'success' | 'warning';
+    [x: string]: any;
+}
+function ErrorGeneric({title, message, children, buttonHandler, buttonText, severity, ...rest}: IProps) {
+    return (
+        <Stack {...rest} alignItems={'center'}>
+            <Stack spacing={'1rem'}>
+                <Alert severity={severity || "info"}>
+                    <AlertTitle>
+                        {title || `An Error Occured`}
+                    </AlertTitle>
+                    {message || <>Please try again later, you may have been redirected here by a broken link.<br/><br/>
+                        Below are some links to help you get back on track.</>}
+                    {children}
+                    <Divider variant={'middle'} sx={{
+                        margin: '1rem 0'
+                    }}/>
+                    <Stack direction={'row'} gap={'1rem'}>
+                        <Button variant={'outlined'}  color={'inherit'} onClick={()=>{
+                            if (buttonHandler) buttonHandler();
+                        }}>
+                            {buttonText || `Return to Home`}
+                        </Button>
+                        <a href={apiURL('')}>
+                            <Button endIcon={<LaunchOutlined/>} color={'inherit'}>
+                                {`Check API Status`}
+                            </Button>
+                        </a>
+                    </Stack>
+                </Alert>
+
+            </Stack>
+        </Stack>
+    );
+}
+
+export default ErrorGeneric;
