012247a7536e7b5ecb68eb7da4319c6828f1152d
Rehman <certifiedrehman@gmail.com>
2022-03-12 22:11:36 -0500

    added multer
    Took 13 seconds

---
+++ api/middleware/multer.middleware.js
@@ -0,0 +1,38 @@
+const aws = require('aws-sdk');
+const multerMiddleware = require('multer')
+const multerS3 = require('multer-s3');
+
+const s3 = new aws.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_ACCESS_KEY
+})
+
+module.exports.deleteFromS3 = (key) => {
+    s3.deleteObject({Bucket: 'restinn', Key: key}, (err, data) => {
+        console.error(err);
+        console.log(data);
+    });
+}
+
+const upload = multerMiddleware({
+    storage: multerS3({
+        s3: s3,
+        bucket: 'restinn',
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        metadata: function (req, file, cb) {
+            cb(null, {fieldName: file.fieldname});
+        },
+        key: function (req, file, cb) {
+            cb(null, Date.now().toString() + '-' + file.originalname)
+        },
+        fileFilter: function (req, file, cb) {
+            if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg' || file.mimetype === 'image/png') {
+                cb(null, true);
+            } else {
+                cb(null, false);
+            }
+        }
+    })
+})
+
+module.exports = upload;
